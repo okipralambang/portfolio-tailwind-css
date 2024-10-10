5233e43d2f598c14aa0cd7a465ded8636aa675bd
it-external16-bfi <it.external16@bfi.co.id>
2024-10-10 13:41:54 +0700

    penambahan scroll behavior

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,16 @@ module.exports = {
       },
       screens:{
         "2xl":"1320px"
-      }
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
